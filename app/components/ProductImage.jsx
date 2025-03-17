c6a9e1b7cf67701db5569b5b5b591a7489e589e4
alexander gubernikoff <agubernikoff@gmail.com>
2025-03-17 18:38:58 -0400

    product images unnecessary cropping

--- app/components/ProductImage.jsx
+++ app/components/ProductImage.jsx
@@ -13,10 +13,11 @@ export function ProductImage({image}) {
     <div className="product-image">
       <Image
         alt={image.altText || 'Product Image'}
-        aspectRatio="1/1"
+        // aspectRatio="1/1"
         data={image}
         key={image.id}
         sizes="(min-width: 45em) 50vw, 100vw"
+        loading="eager"
       />
     </div>
   );
