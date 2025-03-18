ee876368494037fc3ed613dcf05cdfc6f70be43f
alexander gubernikoff <agubernikoff@gmail.com>
2025-03-18 01:34:10 -0400

    default transition speed

--- app/components/ProductGridItem.jsx
+++ app/components/ProductGridItem.jsx
@@ -30,7 +30,6 @@ function ProductGridItem({product, loading}) {
           initial={{opacity: 1}}
           animate={{opacity: 1}}
           exit={{opacity: 0}}
-          transition={{duration: 1}}
         >
           <Image
             alt={image.altText || product.title}
