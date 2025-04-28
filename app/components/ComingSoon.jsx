c22992d05754661f8eaf5f8a09d85c5de3b5da60
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-04-28 16:57:59 -0400

    coming soon mobile

--- app/components/ComingSoon.jsx
+++ app/components/ComingSoon.jsx
@@ -1,22 +1,49 @@
-import React from 'react';
-import {Image} from '@shopify/hydrogen'; // Import the Shopify Image component
+import React, {useState, useEffect} from 'react';
+import {Image} from '@shopify/hydrogen';
 
 function ComingSoon({comingsoon}) {
   const imageUrl = comingsoon?.metaobject?.field?.reference?.image?.url;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 499);
+    };
+
+    checkMobile(); // Initial check
+    window.addEventListener('resize', checkMobile); // Recheck on resize
+
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
 
   return (
     <div className="coming-soon-container">
       <div className="coming-soon-content">
-        <span className="coming-text">coming</span>
-        {imageUrl && (
-          <Image
-            src={imageUrl}
-            alt="Coming Soon"
-            width={450}
-            className="coming-soon-image"
-          />
+        {isMobile ? (
+          <>
+            <div className="coming-soon-mobile-text">coming soon</div>
+            {imageUrl && (
+              <Image
+                src={imageUrl}
+                alt="Coming Soon"
+                className="coming-soon-image-mobile"
+              />
+            )}
+          </>
+        ) : (
+          <>
+            <span className="coming-text">coming</span>
+            {imageUrl && (
+              <Image
+                src={imageUrl}
+                alt="Coming Soon"
+                width={450}
+                className="coming-soon-image"
+              />
+            )}
+            <span className="soon-text">soon</span>
+          </>
         )}
-        <span className="soon-text">soon</span>
       </div>
     </div>
   );
