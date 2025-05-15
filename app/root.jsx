47af03b28703ca2b002a1d15c3076e570e642dc6
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-05-15 15:50:00 -0400

    coming soon

--- app/root.jsx
+++ app/root.jsx
@@ -115,7 +115,8 @@ async function loadCriticalData({context}) {
 
   return {
     header,
-    comingSoon: process.env.NODE_ENV === 'development' ? comingSoon : false,
+    comingSoon:
+      process.env.NODE_ENV === 'development' ? comingSoon : comingSoon,
   };
 }
 
