778e2ce01965ef3b06e2adf9b2afc18684977075
alexander gubernikoff <agubernikoff@gmail.com>
2025-07-10 15:09:01 -0400

    hover grid images fix

--- app/components/ProductGridItem.jsx
+++ app/components/ProductGridItem.jsx
@@ -8,6 +8,7 @@ function ProductGridItem({product, productUrl, loading}) {
   const [image, setImage] = useState(product?.images?.nodes[0]);
   const [hovered, setHovered] = useState(false);
   const variantUrl = useVariantUrl(product.handle);
+  console.log(product.images.nodes);
 
   return (
     <Link
@@ -15,56 +16,26 @@ function ProductGridItem({product, productUrl, loading}) {
       key={product.id}
       prefetch="intent"
       to={productUrl || variantUrl}
-      onMouseEnter={() => {
-        setHovered(true);
-        if (product.images.nodes.length > 1) setImage(product.images.nodes[1]);
-      }}
-      onMouseLeave={() => {
-        setHovered(false);
-        setImage(product.images.nodes[0]);
-      }}
     >
-      <AnimatePresence mode="popLayout">
-        <motion.div
-          key={image.id}
-          initial={{opacity: 1}}
-          animate={{opacity: 1}}
-          exit={{opacity: 0}}
-        >
+      {product?.images?.nodes.map((n) => (
+        <div className="product-item-img-container">
           <Image
-            alt={image.altText || product.title}
+            alt={n.altText || product.title}
             aspectRatio="361/482"
-            data={image}
-            loading={'eager'}
-            sizes="(min-width: 45em) 400px, 100vw"
-          />
-        </motion.div>
-        <div
-          style={{display: 'none'}}
-          key={`idk-${product.images.nodes[1].id}`}
-        >
-          <Image
-            alt={product.images.nodes[1]?.altText || product.title}
-            aspectRatio="361/482"
-            data={product.images.nodes[1]}
+            data={n}
             loading={'eager'}
             sizes="(min-width: 45em) 400px, 100vw"
           />
         </div>
-        <motion.div
-          initial={{opacity: 0}}
-          animate={{
-            opacity: hovered ? 1 : 0,
-          }}
-          exit={{opacity: 0}}
-          transition={{ease: 'linear'}}
-        >
+      ))}
+      <AnimatePresence mode="popLayout">
+        <div className="product-item-title-price">
           <p>{product.title.toLowerCase()}</p>
           <Money
             data={product.priceRange.minVariantPrice}
             withoutTrailingZeros
           />
-        </motion.div>
+        </div>
       </AnimatePresence>
     </Link>
   );
